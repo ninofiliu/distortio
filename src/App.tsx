e833585e4c0023a6b11be8bd7236a5f335e19c22
Nino Filiu <nino.filiu@gmail.com>
2021-10-25 22:50:11 +0200

    main ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+  <div className="app">
+    <img src="//placekitten.com/100/100" alt="" />
+    <div className="input">
+      <h1>Distortio</h1>
+      <h2>I want this...</h2>
+      <input type="file" />
+      <h2>to be distorted by this...</h2>
+      <input type="file" />
+    </div>
+  </div>
+);
